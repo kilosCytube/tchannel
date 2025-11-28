533bc4280e5fde585a534b00fd922125b57a9777
kilosCytube <cytubeanon@gmail.com>
2025-11-28 18:39:13 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
-import DriveSync from "./driveSync.js";
+// src/main.js
+import DriveSync from "./driveSync.js"; // O navegador resolve isso relativo ao arquivo main.js no CDN
 
 window.TChannel = {
     init() {
+        console.log("[TChannel] Inicializando módulos...");
         DriveSync.init();
+    },
+    // Opcional: Expor o módulo para depuração no console
+    modules: {
+        DriveSync
     }
 };
 
-$(document).ready(() => TChannel.init());
+// Inicia quando o DOM estiver pronto (compatível com jQuery do CyTube)
+$(document).ready(() => {
+    window.TChannel.init();
+});
